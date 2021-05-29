7ccf5b7ce0fd5999b97b0128af1a769cb747053b
suryafakhriy <suryafh19@gmail.com>
2021-05-28 23:02:57 -0700

    Initial commit

---
+++ app/src/main/java/com/example/quiz10118028/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.quiz10118028;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+<!--
+            ~     Tanggal Pengerjaan : 29/5/2021
+            ~     Nim   : 10118028
+            ~     Nama  : Surya Fakhriy Hasbi
+            ~     Kelas : IF-1
+            -->
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
